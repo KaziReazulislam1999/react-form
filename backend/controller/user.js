6e7376dc618936a9c1a9fea0b27ac275887b7c8c
Kazi Reazul Islam <kazireazul1999@gmail.com>
2023-09-29 21:01:03 +0600

    added trycatch on controller registerUser

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -4,26 +4,28 @@ const bcrypt = require("bcryptjs");
 exports.registerUser = async (req, res) => {
   const { fName, lName, email, password } = req.body;
 
-  const exist = await User.findOne({ email: email.toLowerCase() }).exec();
+  try {
+    const exist = await User.findOne({ email: email.toLowerCase() }).exec();
 
-  if (exist) {
-    return res.status(400).json({
-      messaged: "Already exist!",
+    if (exist) {
+      return res.status(400).json({
+        messaged: "Already exist!",
+      });
+    }
+
+    const hashPassword = await bcrypt.hash(password, 12);
+
+    const user = await new User({
+      fName,
+      lName,
+      email,
+      password: hashPassword,
+    }).save();
+
+    console.log(user);
+
+    res.status(200).json({
+      message: "Registration success",
     });
-  }
-
-  const hashPassword = await bcrypt.hash(password, 12);
-
-  const user = await new User({
-    fName,
-    lName,
-    email,
-    password: hashPassword,
-  }).save();
-
-  console.log(user);
-
-  res.status(200).json({
-    message: "Registration success",
-  });
+  } catch (error) {}
 };
