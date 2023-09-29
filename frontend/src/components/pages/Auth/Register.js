b94f040f849e620e77b5f41ac6b6501b97b29018
Kazi Reazul Islam <kazireazul1999@gmail.com>
2023-09-29 22:49:36 +0600

    added react-hook-form and implement it

--- frontend/src/components/pages/Auth/Register.js
+++ frontend/src/components/pages/Auth/Register.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
   return (
     <div className="container w-50 my-5">
       <h1 className="text-center">Register</h1>
 
-      <form>
+      <form onSubmit={handleSubmit()}>
         <div className="mb-3">
           <label htmlFor="fName" className="form-label">
             First Name
@@ -16,7 +23,12 @@ const Register = () => {
             className="form-control"
             id="fName"
             placeholder="Enter your first name"
+            name="fName"
+            {...register("fName", { required: "First name is required!" })}
           />
+          {errors.fName && (
+            <p className="text-danger">{errors.message.fName}</p>
+          )}
         </div>
 
         <div className="mb-3">
@@ -28,7 +40,12 @@ const Register = () => {
             className="form-control"
             id="lName"
             placeholder="Enter your last name"
+            name="lName"
+            {...register("lName", { required: "Last name is required!" })}
           />
+          {errors.lName && (
+            <p className="text-danger">{errors.message.lName}</p>
+          )}
         </div>
 
         <div className="mb-3">
@@ -40,7 +57,12 @@ const Register = () => {
             className="form-control"
             id="email"
             placeholder="Enter your email name"
+            name="email"
+            {...register("email", { required: "Email is required!" })}
           />
+          {errors.email && (
+            <p className="text-danger">{errors.message.email}</p>
+          )}
         </div>
 
         <div className="mb-3">
@@ -52,7 +74,12 @@ const Register = () => {
             className="form-control"
             id="password"
             placeholder="Enter your password"
+            name="password"
+            {...register("password", { required: "Password is required!" })}
           />
+          {errors.password && (
+            <p className="text-danger">{errors.message.password}</p>
+          )}
         </div>
         <div className="d-grid gap-2 my-5">
           <button className="btn btn-success">Submit</button>
