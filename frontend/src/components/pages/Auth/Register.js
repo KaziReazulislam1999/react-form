8e112be38fe500e27ddfbc07159e09de534e0d57
Kazi Reazul Islam <kazireazul1999@gmail.com>
2023-09-29 22:35:26 +0600

    design register form

--- frontend/src/components/pages/Auth/Register.js
+++ frontend/src/components/pages/Auth/Register.js
@@ -1,7 +1,69 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="container w-50 my-5">
+      <h1 className="text-center">Register</h1>
+
+      <form>
+        <div className="mb-3">
+          <label htmlFor="fName" className="form-label">
+            First Name
+          </label>
+          <input
+            type="fName"
+            className="form-control"
+            id="fName"
+            placeholder="Enter your first name"
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="lName" className="form-label">
+            Last Name
+          </label>
+          <input
+            type="lName"
+            className="form-control"
+            id="lName"
+            placeholder="Enter your last name"
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            placeholder="Enter your email name"
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            placeholder="Enter your password"
+          />
+        </div>
+        <div className="d-grid gap-2 my-5">
+          <button className="btn btn-success">Submit</button>
+        </div>
+      </form>
+      <div className="text-center">
+        <p>Already have account!</p>
+        <Link to="/login">Login here</Link>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
